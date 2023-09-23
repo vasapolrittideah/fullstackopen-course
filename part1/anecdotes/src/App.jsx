3034dca21abe93da8346654da6a675bafc711b47
Vasapol Rittideah <vasapol.rittideah@outlook.com>
2023-09-23 18:33:34 +0700

    part1: implement anecdotes part

---
+++ part1/anecdotes/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+const App = () => {
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
+
+  const handleNextAnecdoteClick = () =>
+    selected === setSelected(Math.floor(Math.random() * anecdotes.length));
+
+  const handleVoteClick = () => {
+    const copyPoints = [...points];
+    copyPoints[selected] += 1;
+    setPoints(copyPoints);
+  };
+
+  const getIndexOfMaxPoints = () => points.indexOf(Math.max(...points));
+
+  return (
+    <>
+      <h1>Anecdote of the day</h1>
+      <div>{anecdotes[selected]}</div>
+      <div>has {points[selected]} votes</div>
+      <div>
+        <button onClick={handleVoteClick}>vote</button>
+        <button onClick={handleNextAnecdoteClick}>next anecdote</button>
+      </div>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[getIndexOfMaxPoints()]}</div>
+      <div>has {points[getIndexOfMaxPoints()]} votes</div>
+    </>
+  );
+};
+
+export default App;
